56e3668769fa7048d722436d8bcc143215d4bbcf
smchio99-sudo <smchio99@gmail.com>
2026-06-02 16:35:59 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase Console > Project settings > Your apps > Web app config.
+// Fill these values, then set STUDY_QUEST_USE_FIREBASE to true.
+window.STUDY_QUEST_USE_FIREBASE = true;
+
+window.STUDY_QUEST_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAYMYBLgWGf-GZW0DBNSyHEJUgSUvjwObY",
+  authDomain: "studyquest-8dce1.firebaseapp.com",
+  projectId: "studyquest-8dce1",
+  storageBucket: "studyquest-8dce1.firebasestorage.app",
+  messagingSenderId: "872083492736",
+  appId: "1:872083492736:web:6e7f619438434e2cdf7b65"
+};
